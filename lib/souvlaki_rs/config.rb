fa064b7f3004e61efa225a5897a63d65da9a55e4
Ed Porras <technical@wgot.org>
2016-06-12 13:21:02 +0200

    check for existing config file and exit with error if not found

--- lib/souvlaki_rs/config.rb
+++ lib/souvlaki_rs/config.rb
@@ -4,24 +4,39 @@ require_relative 'log'
 module SouvlakiRS
 
   module Config
-    CONFIG_FILE = File.join( ENV['HOME'], '.souvlaki_rs') # file in EDN format
+    PATH = ENV['HOME']
+    FILE = File.join( PATH, '.souvlaki_rs') # file in EDN format
+
+    def self.exist?
+      File.exist? FILE
+    end
+
+    def self.list_program_codes
+      pc = SouvlakiRS::Config.get_program_info
+      $stderr.puts "Configured code List:"
+      pc.each_pair { |code,data| $stderr.puts " #{code}\t-\t'#{data[:pub_title]}'" }
+    end
 
     def self.get_host_info(host)
-      get_entry(host)
+      return get_entry(host) if exist?
+      nil
     end
 
     def self.get_program_info(code = nil)
-      progs = get_entry(:programs)
+      if exist?
+        progs = get_entry(:programs)
 
-      if progs
-        return progs if code == nil
-        return progs[code] if progs.key?(code)
+        if progs
+          return progs if code == nil
+          return progs[code] if progs.key?(code)
+        end
       end
       nil
     end
 
     def self.get_dropbox_folder_info()
-      get_entry(:dropbox)
+      return get_entry(:dropbox) if exist?
+      nil
     end
 
 
@@ -32,13 +47,10 @@ module SouvlakiRS
     def self.get_entry(e)
 
       if @@data == nil
-        if !File.exist? CONFIG_FILE
-          SouvlakiRS::logger.error "Config File '#{CONFIG_FILE}' not found"
-          return nil
-        end
+        return nil if !exist?
 
         # read contents
-        File.open(CONFIG_FILE) do |file|
+        File.open(FILE) do |file|
           @@data = EDN::read(file)
         end
       end
@@ -48,6 +60,13 @@ module SouvlakiRS
       nil
     end
 
+    def self.exist?
+      if !File.exist? FILE
+        $stderr.puts "Configuration file not found. See config.example and save it as #{FILE} once set up for your needs."
+        exit
+      end
+      true
+    end
   end
 
 end
