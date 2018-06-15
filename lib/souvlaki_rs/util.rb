3fa648de198023c4c0ad6149b0df60d2cd41d7b0
Ed Porras (WGOT) <ep@digressed.net>
2018-06-15 14:59:32 -0400

    cleanup

--- lib/souvlaki_rs/util.rb
+++ lib/souvlaki_rs/util.rb
@@ -3,7 +3,7 @@ require 'filemagic'
 require 'uri'
 
 module SouvlakiRS
-
+  # utilities
   module Util
     #
     # joins AIRTIME_ROOT with a subfolder for copying files to
@@ -14,19 +14,18 @@ module SouvlakiRS
     #
     # tmp file
     def self.get_tmp_path(name = nil)
-      return TMP_DIR_PATH if name == nil
+      return TMP_DIR_PATH if name.nil?
       File.join(TMP_DIR_PATH, name)
     end
 
     #
     # ensure dest directory exists
     def self.check_destination(path, opts = {})
-
-      if !Dir.exist?(path)
+      unless Dir.exist?(path)
         begin
           FileUtils.mkdir_p(path, opts)
         rescue Errno::ENOENT
-          SouvlakiRS::logger.error "Error making directory #{path}"
+          SouvlakiRS.logger.error "Error making directory #{path}"
           return false
         end
       end
@@ -36,13 +35,10 @@ module SouvlakiRS
 
     #
     # check that the path looks like an mp3 file
-    @@fm = FileMagic.new
-
     def self.get_type_desc(file)
       if File.exist?(file) && File.size(file) > 10
         # checks if the type description contains 'MP3' or 'MPEG'
-        return @@fm.file(file)
-        re
+        return FileMagic.new.file(file)
       end
       nil
     end
@@ -56,9 +52,7 @@ module SouvlakiRS
     #
     # check for valid URI
     def self.valid_uri?(uri)
-      uri =~ /\A#{URI::regexp}\z/
+      uri =~ /\A#{URI::DEFAULT_PARSER.make_regexp}\z/
     end
-
   end
-
 end
