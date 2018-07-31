e2e583db16f70c6decf0d84a170fb4eb8e43f7ef
Ed Porras (WGOT) <ep@digressed.net>
2018-07-31 02:09:14 -0400

    rubocop'd

--- lib/souvlaki_rs.rb
+++ lib/souvlaki_rs.rb
@@ -8,37 +8,36 @@ require 'souvlaki_rs/mail'
 require 'souvlaki_rs/tag'
 require 'souvlaki_rs/util'
 require 'souvlaki_rs/version'
+require 'pry'
 
+#
+# SouvlakiRS module
 module SouvlakiRS
-
-#  TMP_DIR_PATH=File.join(File.expand_path('~'), 'tmp')
-  TMP_DIR_PATH=File.join('/srv/incoming')
-#  AIRTIME_ROOT='/srv/incoming'
-  AIRTIME_ROOT=TMP_DIR_PATH
+  #  TMP_DIR_PATH=File.join(File.expand_path('~'), 'tmp')
+  TMP_DIR_PATH = File.join('/srv/incoming').freeze
+  #  AIRTIME_ROOT='/srv/incoming'
+  AIRTIME_ROOT = TMP_DIR_PATH
 
   # ------------------------------------------------------------------------
   # download and import the program's episode that matches the given
   # date. Post notification on basecamp
   #
   def self.audioport_download(program, date, show_name_uri)
-
     files = []
-    show_dir  = SouvlakiRS::Util.get_show_path(program)
+    show_dir = SouvlakiRS::Util.get_show_path(program)
 
     # ensure the destination directory exists
     if SouvlakiRS::Util.check_destination(show_dir)
 
       # spider audioport and download any files we find that match the date
       files = SouvlakiRS::Audioport.fetch_files(program, date, show_name_uri)
+binding.pry
 
-      if files.empty?
-        SouvlakiRS::logger.warn "Unable to download '#{program}' dated #{date} from Audioport"
-      end
+      SouvlakiRS.logger.warn "Unable to download '#{program}' dated #{date} from Audioport" if files.empty?
     end
 
     files
   end
-
 
   # ------------------------------------------------------------------------
   # parse the given RSS feed, find the program's entry(ies) that
@@ -46,7 +45,6 @@ module SouvlakiRS
   # basecamp
   #
   def self.rss_download(program, rss_uri, date)
-
     files = []
     mp3_uri = SouvlakiRS::Fetch.find_rss_entry(rss_uri, date)
 
@@ -54,35 +52,31 @@ module SouvlakiRS
       # we have the uri and the destination - fetch the audio file
       files = remote_file_download(program, mp3_uri)
     else
-      SouvlakiRS::logger.error "Unable to find RSS entry in #{rss_uri} for '#{program}', date: #{date}"
+      SouvlakiRS.logger.error "Unable to find RSS entry in #{rss_uri} for '#{program}', date: #{date}"
     end
 
     files
   end
 
-
   # ------------------------------------------------------------------------
   # fetch the file pointed to by uri
   #
   def self.remote_file_download(program, uri)
-
     files = []
-    show_dir  = SouvlakiRS::Util.get_show_path(program)
+    show_dir = SouvlakiRS::Util.get_show_path(program)
 
     # determine a file destination and ensure the directory exists
     if SouvlakiRS::Util.check_destination(show_dir)
-
       mp3_dest = File.join(show_dir, File.basename(uri))
 
       # try to download
       if SouvlakiRS::Fetch.fetch_file(uri, mp3_dest)
         files << mp3_dest
       else
-        SouvlakiRS::logger.error "Unable to download '#{program}' from #{uri}"
+        SouvlakiRS.logger.error "Unable to download '#{program}' from #{uri}"
       end
     end
 
     files
   end
-
 end
--- lib/souvlaki_rs/audioport.rb
+++ lib/souvlaki_rs/audioport.rb
@@ -1,47 +1,56 @@
+# -*- coding: utf-8 -*-
 require 'fileutils'
 require 'mechanize'
 require_relative 'config'
 
 module SouvlakiRS
-
+  #
+  # scraping AudioPort.org
   module Audioport
-
-    @@creds = SouvlakiRS::Config.get_host_info(:audioport)
-    @@mm = nil
-    @@TMP_DIR = nil
+    @creds = SouvlakiRS::Config.get_host_info(:audioport)
+    @mm = nil
+    @TMP_DIR = nil
 
     # ====================================================================
     # returns the audioport spider agent instance, initializing it and
     # login in when invoked for the first time
     def self.spider
-
-      if @@mm == nil
-
-        @@TMP_DIR = SouvlakiRS::Util.get_tmp_path("audioport")
+      if @mm.nil?
+        @TMP_DIR = SouvlakiRS::Util.get_tmp_path('audioport')
 
         # initialize and log in
-        @@mm = Mechanize.new { |agent|
-          agent.user_agent_alias = 'Mac Safari'
-#          agent.pluggable_parser['audio/mpeg'] = Mechanize::DirectorySaver.save_to(SouvlakiRS::Util.get_tmp_path)
-        }
+        @mm = Mechanize.new do |agent|
+          agent.user_agent_alias = 'Windows Mozilla'
+          agent.follow_meta_refresh = true
+          agent.redirect_ok = true
+          agent.keep_alive = true
+          agent.open_timeout = 30
+          agent.read_timeout = 30
+          #          agent.pluggable_parser['audio/mpeg'] = Mechanize::DirectorySaver.save_to(SouvlakiRS::Util.get_tmp_path)
+        end
 
         # get the login page
-        uri = "#{@@creds[:base_uri]}?op=login&amp;"
-        @@mm.get(uri)
+        uri = "#{@creds[:base_uri]}?op=login&amp;"
+        @mm.get(uri)
 
         # There are two forms on the page with the same script:
         # - GET form for search
         # - POST form for login in
         # Submit the login form :)
-        @@mm.page.form_with(:method => 'POST') do |f|
-          f.email    = @@creds[:username]
-          f.password = @@creds[:password]
+        @mm.page.form_with(method: 'POST') do |f|
+          f.email    = @creds[:username]
+          f.password = @creds[:password]
         end.submit
 
-        SouvlakiRS::logger.info "Audioport user logged in"
+        if !@mm.page.content.include? 'You are logged in.'
+          SouvlakiRS.logger.error 'Audioport user login failed'
+          raise 'Couldn\'t log in'
+        end
+
+        SouvlakiRS.logger.info 'Audioport user logged in'
       end
 
-      @@mm
+      @mm
     end
 
     # ====================================================================
@@ -51,16 +60,16 @@ module SouvlakiRS
       files = []
 
       # audioport date format
-      show_date = date.strftime("%Y-%m-%d")
+      show_date = date.strftime('%Y-%m-%d')
 
-      SouvlakiRS::logger.info "Audioport fetch for '#{show_name}', date: #{show_date}"
+      SouvlakiRS.logger.info "Audioport fetch for '#{show_name}', date: #{show_date}"
 
       # go to the show's page
-      uri = "#{@@creds[:base_uri]}?op=series&amp;series=#{show_name_uri}"
+      uri = "#{@creds[:base_uri]}?op=series&amp;series=#{show_name_uri}"
 
       begin
         spider.get(uri)
-        SouvlakiRS::logger.info "fetched '#{uri}', status code: #{spider.page.code.to_i}"
+        SouvlakiRS.logger.info "fetched '#{uri}', status code: #{spider.page.code.to_i}"
 
         # we have the page of results. Audioport formats this a little funky:
         # <div id="result_set">
@@ -70,71 +79,70 @@ module SouvlakiRS
         #     <tr><td></td><td>producer</td><td>date (YYYY-MM-DD format)</td><td>length</td></tr>
         #
         # start by getting the list of tr
-        trs = spider.page.search("//div[@id='result_set']").search('tr')
+        trs = spider.page.search('//div[@id="result_set"]').search('tr')
 
         ii = 2
-        while ii < trs.length do
+        while ii < trs.length
           # therefore, we find each entry every-other one starting from
           # the 3rd TR and then fetch the 3rd td to extract the date -
           # also, replace \u00a0 for &nbsp; and strip to remove
           # surrounding whitespace
-          date = trs[ii].search('td')[2].text.gsub(/\u00A0/, '').strip
+          date = trs[ii].search('td')[2].text.delete(' ').strip
 
           break if date < show_date
 
-          SouvlakiRS::logger.info " TR #{ii} is #{date}"
+          SouvlakiRS.logger.info " TR #{ii} is #{date}"
 
           if date == show_date
             # we have a match so we must follow to the next page which
             # actually carries the link to the MP3. Grab the first link to
             # the programs that corresponds to the entry we matched
             links = spider.page.links
-            uris = links.select { |l| l.href.include? "program-info" if l.href }
-            uri = uris[(ii/2)-1]
+            uris = links.select { |l| l.href.include? 'program-info' if l.href }
+            uri = uris[(ii / 2) - 1]
 
-            SouvlakiRS::logger.info "date match (#{date}) - following #{uri.href}"
+            SouvlakiRS.logger.info "date match (#{date}) - following #{uri.href}"
             uri.click
 
             # we're now on the final page that contains the link(s) to
             # download - look by searching for 'file_id'.
-            uri_list = spider.page.links.select { |l| l.href.include? "file_id" if l.href }
+            uri_list = spider.page.links.select { |l| l.href.include? 'file_id' if l.href }
 
-            if uri_list.empty?
-              SouvlakiRS::logger.warn "No entries found for date"
-            end
+            SouvlakiRS.logger.warn 'No entries found for date' if uri_list.empty?
 
             jj = 0
             uri_list.each do |l|
-
-              SouvlakiRS::logger.info "starting download for #{l.href}"
+              SouvlakiRS.logger.info "starting download for #{l.href}"
 
               # fetch the data
-              data = spider.get( l.href )
+              data = spider.get(l.href)
               if data
                 # filename might be in header's content-disposition
-                filename=''
-                cd = data.header['content-disposition'].split("=")
-                if cd.length > 1
-                  # extract the filename
-                  filename = cd[1].gsub(/\A"|"\Z/, '')
-                  filename = "#{filename.slice(0..(filename.downcase.index('.mp3')))}mp3"
+                filename = ''
+                if data.header['content-disposition']
+                  cd = data.header['content-disposition'].split('=')
+                  if cd.length > 1
+                    # extract the filename
+                    path = cd[1].gsub(/\A"|"\Z/, '')
+                    filename = "#{path.slice(0..(path.downcase.index('.mp3')))}mp3"
+                  end
                 end
 
-                SouvlakiRS::logger.info "Header content-disposition: #{filename}"
+                SouvlakiRS.logger.info "Header content-disposition: #{filename}"
 
-                if filename.length == 0
+                if filename.empty?
                   # not included - make one up
                   filename = "#{show_date}-#{show_name}-#{jj}.mp3"
                 end
 
-                dest_file = File.join(@@TMP_DIR, filename)
+                dest_file = File.join(@TMP_DIR, filename)
 
                 # delete if it exists
                 FileUtils.rm_f(dest_file) if File.exist?(dest_file)
 
                 data.save_as(dest_file)
 
-                SouvlakiRS::logger.info "File saved: #{dest_file}"
+                SouvlakiRS.logger.info "File saved: #{dest_file}"
 
                 # either downloaded it or file was there already
                 files << dest_file
@@ -145,28 +153,27 @@ module SouvlakiRS
                 # perhaps check label?
                 break
               else
-                SouvlakiRS::logger.error "download failed"
+                SouvlakiRS.logger.error 'download failed'
               end
 
               jj += 1
             end
-
           end
 
           ii += 2
         end
-      rescue #Timeout::Error
-        SouvlakiRS::logger.error "Connection timeout error when fetching \"#{uri}\"."
+      rescue StandardError => e
+        SouvlakiRS.logger.error "Error when fetching \"#{uri}\": #{e}"
         return nil
       end
 
       # logout
-#      uri = "#{@@creds[:base_uri]}?op=logout&amp;"
+      logout_btn = spider.page.link_with(text: "Logout")
+      logout_btn.click unless logout_btn.nil?
+#      uri = "#{@creds[:base_uri]}?op=logout&amp;"
 #      spider.get(uri)
 
       files
     end
-
   end
-
 end
